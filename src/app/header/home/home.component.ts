f7441119b13cf4c413d3f460404d8c7fdf88bae7
Priya <pkhilosiya12@gmail.com>
2025-08-01 17:04:08 +0530

    setup

---
+++ src/app/header/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeroComponent } from "./hero/hero.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeroComponent, SidebarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
